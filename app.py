be840cd0e1e4bd11609873b6aeb283b96ea626d0
sarathrsk03 <sarathrajansk@gmail.com>
2024-01-29 21:28:36 +0530

    Commit 2

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-#Import the required modules
-from flask import Flask, request, jsonify, make_response, render_template, session, flash
+from flask import Flask, request, jsonify, make_response, render_template, session
 import jwt
 from datetime import datetime, timedelta
 from functools import wraps
@@ -7,8 +6,7 @@ from functools import wraps
 app = Flask(__name__)
 
 # Secret key for encoding and decoding JWT tokens
-app.config['SECRET_KEY'] = '8asbddfgbfgdkufghdukffcshkdiwsujd' # In real life will be using env variables to define the secret key
-
+app.config['SECRET_KEY'] = 'dbfhdsrhywe1293u234ruy43ruy4rsd'
 
 def token_required(func):
     """
@@ -16,26 +14,24 @@ def token_required(func):
     """
     @wraps(func)
     def decorated(*args, **kwargs):
-        # Extract the token from the request parameters
         token = request.args.get('token')
         if not token:
-            return jsonify({'Alert!': 'Token is missing!'}), 401 # Unauthorised HTTP status code
+            return jsonify({'Alert!': 'Token is missing!'}), 401  # Unauthorized HTTP status code
 
         try:
             # Decode and verify the JWT token
             data = jwt.decode(token, app.config['SECRET_KEY'])
         except jwt.ExpiredSignatureError:
             # Token has expired
-            return jsonify({'Message': 'Token has expired'}), 403 # Forbidden HTTP status code
+            return jsonify({'Message': 'Token has expired'}), 403  # Forbidden HTTP status code
         except jwt.InvalidTokenError:
             # Invalid token
-            return jsonify({'Message': 'Invalid token'}), 403 # Forbidden HTTP status code
+            return jsonify({'Message': 'Invalid token'}), 403  # Forbidden HTTP status code
 
         # Call the original function with the decoded data
         return func(*args, **kwargs)
 
     return decorated
-
 
 @app.route('/')
 def home():
@@ -46,8 +42,7 @@ def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return 'logged in currently'
-
+        return 'Logged in currently'
 
 @app.route('/public')
 def public():
@@ -56,7 +51,6 @@ def public():
     """
     return 'For Public'
 
-
 @app.route('/auth')
 @token_required
 def auth():
@@ -64,7 +58,6 @@ def auth():
     Protected route that requires a valid JWT token for access.
     """
     return 'JWT is verified. Welcome to your dashboard!'
-
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -81,21 +74,18 @@ def login():
             'expiration': str(datetime.utcnow() + timedelta(seconds=60))
         },
             app.config['SECRET_KEY'])
-
-        # Return the token in the response
-        return jsonify({'token': token.decode('utf-8')})
+        return jsonify({'token': token})
     else:
         # Return a 403 error if the username and password are incorrect
         return make_response('Unable to verify', 403, {'WWW-Authenticate': 'Basic realm: "Authentication Failed "'})
 
-
 @app.route('/logout', methods=['POST'])
 def logout():
     """
-    Placeholder for a logout route. The actual logout functionality can be implemented here.
+    Route for user logout. It clears the 'logged_in' session variable.
     """
-    pass
-    # your code goes here
+    session.pop('logged_in', None)
+    return 'Logged out'
 
 if __name__ == "__main__":
     app.run(debug=True)
--- readMe.md
+++ readMe.md
@@ -1,5 +1,9 @@
 Run the following commands before executing the program 
 
+mkdir [Filename]
+
+Copy the Folder's to the newly created directory
+
 python3 -m venv env // Creates a new virtual python environmenmt 
 
 source venv/bin/activate // To activate the virtual python environment 
@@ -7,5 +11,9 @@ source venv/bin/activate // To activate the virtual python environment
 which python // Used to verify whether the virtual env is currently being used 
 
 pip install -r requirements.txt // To install all the required python libraries 
+
+python3 app.py // To run the Flask server 
+                or  
+python app.py //To run the Flask server 
 
 
